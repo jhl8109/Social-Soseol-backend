27778cb7be4664d9a607b8ebd131a0faedb3baea
제호 이 <jhl81094@gmail.com>
2022-03-22 21:01:17 +0900

    boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Social-Soseol-backend
+
 사회가 소설을 만든다.
---
+++ src/main/java/cse/soseol/socialsoseolbackend/SocialSoseolBackendApplication.java
@@ -0,0 +1,13 @@
+package cse.soseol.socialsoseolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSoseolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialSoseolBackendApplication.class, args);
+    }
+
+}
